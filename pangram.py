a818ae71b41c211845a073db1ca323f5245f37a7
Tacenda9999 <tacenda9999@gmail.com>
2025-03-11 22:57:33 +0900

    Add files via upload

--- pangram.py
+++ pangram.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 from fontTools.ttLib import TTFont
+import re
 
 PANGRAM = """아앙 하악 흐읏 큭 간다 간닷
 움찔 흠칫 두근 주르륵 끈적 질척 찔꺽
@@ -18,22 +19,26 @@ TEXT_COLOR = "black"
 BACKGROUND_COLOR = "white"
 FONT_DIR = "Fonts"
 OUTPUT_DIR = "Pangrams"
-FONTSIZE = 30
+FONTSIZE = 10
 LINE_SPACE = int(FONTSIZE * 2/3)
 
 
 def get_font_name(font_path):
     font = TTFont(font_path)
-    name_table = font['name']
-    
-    for info in name_table.names:
-        # 4 : Font Full Name
-        if info.nameID == 4:
-            font_name = info.toStr()
-            # 한글명 추출
-            if any('\uac00' <= char <= '\ud7af' for char in font_name):
-                return font_name
+    name_table = font["name"]
+
+    for name in name_table.names:
+        try:
+            # 4번 : 폰트 풀네임
+            if name.nameID == 4:
+                # platformID : 0-유니코드, 1-맥, 2-ISO, 3-윈도우
+                if name.platformID == 3:  
+                    font_name = name.string.decode("utf-16-be", errors="ignore")
+        except Exception:
+            continue
+
     return font_name
+
 
 if __name__ == "__main__":
     if not os.path.exists(OUTPUT_DIR):
@@ -56,12 +61,7 @@ if __name__ == "__main__":
             img = Image.new("RGB", IMAGE_SIZE, BACKGROUND_COLOR)
             draw = ImageDraw.Draw(img)
             
-            # 폰트 이름 : 우측상단
-            font_name_bbox = draw.textbbox((0, 0), f"Font: {font_name}", font=font_small)
-            font_name_width = font_name_bbox[2] - font_name_bbox[0]
-            font_name_position = (IMAGE_SIZE[0] - font_name_width - FONT_NAME_MARGIN[0], FONT_NAME_MARGIN[1])
-            
-            # 텍스트 배치
+            # 팬그램 작성
             lines = PANGRAM.splitlines()
             y_position = TEXT_POSITION[1]
             for line in lines:
@@ -70,15 +70,28 @@ if __name__ == "__main__":
                 draw.text((TEXT_POSITION[0], y_position), line, font=font, fill=TEXT_COLOR)
                 y_position += text_height + LINE_SPACE
             
-            # 텍스트 작성
+            # 폰트 이름 작성
+            font_name_bbox = draw.textbbox((0, 0), f"Font: {font_name}", font=font_small)
+            font_name_width = font_name_bbox[2] - font_name_bbox[0]
+            font_name_position = (IMAGE_SIZE[0] - font_name_width - FONT_NAME_MARGIN[0], FONT_NAME_MARGIN[1])
             draw.text(font_name_position, f"{font_name}", font=font_small, fill=TEXT_COLOR)
             
-            # 저장
-            output_path = os.path.join(OUTPUT_DIR, f"{font_name}.png")
-            img.save(output_path)
-            print(f"[{cnt}]✅ {output_path}")
+            # 저장            
+            ## 폰트명 깨짐 확인
+            if re.search(r'[가-힣A-Za-z]', font_name):
+                output_path = os.path.join(OUTPUT_DIR, f"{font_name}.png")
+                img.save(output_path)
+                print(f"[{cnt}]✅ {output_path}")
+            else:
+                filename = os.path.splitext(os.path.basename(font_path))[0]
+                font_name = f"{filename}_ERROR"
+                output_path = os.path.join(OUTPUT_DIR, f"{font_name}.png")
+                img.save(output_path)
+                print(f"[{cnt}]❌ {font_name} - 폰트 인식 실패")
+            
         
         except Exception as e:
             print(f"[{cnt}]❌ {font_name}: {e}")
+            continue
 
     print(f"{cnt}개 생성")
